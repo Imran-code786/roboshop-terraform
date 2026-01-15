e74bb603a39650053519a3ea2223fe1c2dd44ea5
Imran <imrankhanmohammed130@gmail.com>
2026-01-15 13:21:51 -0600

    vvv

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
-module "test" {
-  source = "git::https://github.com/Imran-code786/tf-module-app.git"
-  env = "dev"
 
+module "test"{
+ source = "git::https://github.com/Imran-code786/tf-module-app.git"
+   env = "dev"
 }
+
+
