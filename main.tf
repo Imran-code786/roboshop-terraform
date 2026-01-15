e25ab067495475a334fa66d0f5b1a8ad3fef5500
Imran <imrankhanmohammed130@gmail.com>
2026-01-15 13:30:37 -0600

    as

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-
-module "test"{
- source = "git::https://github.com/Imran-code786/tf-module-app.git"
-   env = "dev"
+module "test" {
+  source = "git::ssh://git@github.com/Imran-code786/tf-module-app.git"
+  env    = "dev"
 }
-
-
