c1a35c0f0422e7d2cab082a676386024d08b9f66
Imran <imrankhanmohammed130@gmail.com>
2026-01-15 16:10:19 -0600

    scx

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
-module "test" {
-  source = "git::https://github.com/Imran-code786/tf-module-app.git"
-  env    = var.env
+module "instance" {
+    for_each  = var.components
+    source  = "git::https://github.com/Imran-code786/tf-module-app.git"
+    component  = each.key
+    env = var.env
+
 }
